897f41cc3ae5428008a812e6cf564ac6d80d9016
Azmir Suljic <88405828+azmir0218@users.noreply.github.com>
2022-01-15 14:24:29 -0500

    moving files to home location

---
+++ data/SQLTable/postgresfile.sql
@@ -0,0 +1,15 @@
+drop table if exists storm;
+create table storm (
+	Latitude varchar,
+	Longitude varchar,
+	State varchar,
+	Name varchar,
+	Disaster varchar,
+	Year_start varchar,
+	Year_end varchar,
+	Total_dam varchar,
+	Deaths varchar
+	
+);
+
+select * from storm
